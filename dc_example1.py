022279ec0d0bcae19f3975d1b5a71f781383ab5d
David <david.04.fsl@gmail.com>
2023-06-06 16:05:16 +0200

    .

--- dc_example1.py
+++ dc_example1.py
@@ -1,38 +1,64 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-   Servo Example - Example of usage ASMpi class
-.. Licence MIT
-.. codeauthor:: Jan Lipovský <janlipovsky@gmail.com>, janlipovsky.cz
-"""
+
 
 from AMSpi import AMSpi
 import time
 
-black = 1
-white = 0
-other = 0
-last_disk = 'black'
+with AMSpi() as amspi:
 
-if __name__ == '__main__':
-    # Calling AMSpi() we will use default pin numbering: BCM (use GPIO numbers)
-    # if you want to use BOARD numbering do this: "with AMSpi(True) as amspi:"
-    with AMSpi() as amspi:
-        
-        # Set PINs for controlling shift register (GPIO numbering)
-        
-        amspi.set_74HC595_pins(21, 20, 16)
-        
-        # Set PINs for controlling all 4 motors (GPIO numbering)
-        
-        amspi.set_L293D_pins(5, 6, 13, 19)
+#find which motor corresponds to which?
+    push_motor, conveyer_belt, bucket = amspi.DC_Motor_1, amspi.DC_Motor_2, amspi.DC_Motor_3
 
-        if black == 1:
-            
+    # Set PINs for controlling shift register (GPIO numbering)
+    amspi.set_74HC595_pins(21, 20, 16)
+
+    # Set PINs for controlling all 4 motors (GPIO numbering)
+
+    amspi.set_L293D_pins(5, 6, 13, 19)
+
+    def read_motion():
+         pass
+    def reset_pushing_mechanism():
+        #find the exact configuration to reste the pushing mechanism
+        amspi.run_dc_motors(push_motor)
+        time.sleep(5)
+        amspi.stop_dc_motors(push_motor)
+
+    def motor_fetch():
+        #fetch the disk
+        amspi.run_dc_motors(push_motor, clockwise=True)
+        time.sleep(5)
+        amspi.stop_dc_motors(push_motor)
 
 
-            print("GO: clockwise with 50% of maximum speed")
-            amspi.run_dc_motors([amspi.DC_Motor_1], speed=52)
-            print("Stop")
-            amspi.stop_dc_motors([amspi.DC_Motor_1])
-            time.sleep(2)
+    def conveyer_belt():
+        #read color sensor value and call bucket_sorter with the propper signal
+        amspi.run_dc_motors(conveyer_belt)
+        time.sleep(5)
+        amspi.stop_dc_motors(conveyer_belt)
+
+    def color_sensor():
+        pass
+    def bucket_sorter():
+         pass
+
+#constantly listen for motion
+while True:
+    # listen for motion
+    motion = read_motion()
+
+    # When motion is sensed
+    if motion:
+        #we only keep listening for motion when the disk is processed
+        time.sleep(5)
+        motor_fetch()
+        conveyer_belt()
+        bucket_sorter()
+        #Reset the fetching mechanism to its initial position when track is clear
+        while True:
+             motion = read_motion()
+             if motion == 0:
+                  reset_pushing_mechanism()
+                  break
+
