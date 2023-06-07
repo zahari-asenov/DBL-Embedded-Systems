c7daf4f5b51da18c418c0905ca45d82c5f24890d
David <david.04.fsl@gmail.com>
2023-06-07 11:31:49 +0200

    a

--- fetch.py
+++ fetch.py
@@ -1,20 +1,21 @@
 from AMSpi import AMSpi
 import time
 
-with AMSpi() as amspi:
 
-	#find which motor corresponds to which?
-	push_motor, conveyer_belt, bucket = amspi.DC_Motor_1, amspi.DC_Motor_2, amspi.DC_Motor_3
 
-	# Set PINs for controlling shift register (GPIO numbering)
-	amspi.set_74HC595_pins(21, 20, 16)
+if __name__ == '__main__':
+	with AMSpi() as amspi:
 
-	# Set PINs for controlling all 4 motors (GPIO numbering)
+		#find which motor corresponds to which?
+		push_motor, conveyer_belt, bucket = amspi.DC_Motor_1, amspi.DC_Motor_2, amspi.DC_Motor_3
 
-	amspi.set_L293D_pins(5, 6, 13, 19)
+		# Set PINs for controlling shift register (GPIO numbering)
+		amspi.set_74HC595_pins(21, 20, 16)
 
-	def motor_fetch():
-		#fetch the disk
+		# Set PINs for controlling all 4 motors (GPIO numbering)
+
+		amspi.set_L293D_pins(5, 6, 13, 19)
+
 		amspi.run_dc_motors(push_motor)
 		time.sleep(5)
 		amspi.stop_dc_motors(push_motor)
@@ -27,4 +28,3 @@ with AMSpi() as amspi:
 		time.sleep(5)
 		amspi.stop_dc_motor(bucket)
 
-	motor_fetch()
