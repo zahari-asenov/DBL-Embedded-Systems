6f4a8900ad5dc9fad4b83f064ba1783a483117e0
David <david.04.fsl@gmail.com>
2023-06-17 17:28:52 +0200

    .

--- brain.py
+++ brain.py
@@ -1,7 +1,7 @@
 import serial
 import time
 from distance_ready import motion
-from colorsensor import get_color
+from color import get_color
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
