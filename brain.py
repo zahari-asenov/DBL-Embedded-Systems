7f554ba65d033fe5657e2c74f1bced9491e90469
David <david.04.fsl@gmail.com>
2023-06-17 17:53:22 +0200

    .

--- brain.py
+++ brain.py
@@ -1,7 +1,7 @@
 import serial
 import time
-from distance_ready import get_motion
-from color import get_color
+import distance
+import color
 
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 ser.reset_input_buffer()
@@ -11,12 +11,12 @@ while True:
 	color = ""
 	received_data = False
 	while motion == False:
-		motion = get_motion()
+		motion = distance.get_motion()
 	#when motion is detected
 	ser.write(b"M")
 	#wait until disk is under light sensor
 	time.sleep(12.5)
-	color = get_color()
+	color = color.get_color()
 	ser.write(color.encode())
 	#wait for instructions from the arduino
 	while received_data == False:
@@ -27,7 +27,7 @@ while True:
 
 	if data == "check if track is clear":
 		while motion == True:
-			motion = get_motion()
+			motion = distance.get_motion()
 		#Tell the arduino that track is clear ->  Go extend fetching mechanism
 		ser.write(b"G")
 
