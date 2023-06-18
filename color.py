e78e460ebb56392b94487944d1b51e31c60eafb6
sotirisxar <135531361+sotirisxar@users.noreply.github.com>
2023-06-18 17:22:37 +0200

    Add files via upload

--- color.py
+++ color.py
@@ -2,12 +2,12 @@ import RPi.GPIO as GPIO
 import time
 from collections import Counter
 
-
 s2 = 23
 s3 = 24
 signal = 25
 NUM_CYCLES = 10
-NUM_MEASUREMENTS = 6
+NUM_MEASUREMENTS = 8
+
 
 def setup():
     GPIO.setmode(GPIO.BCM)
@@ -15,6 +15,7 @@ def setup():
     GPIO.setup(s2, GPIO.OUT)
     GPIO.setup(s3, GPIO.OUT)
     print("\n")
+
 
 def get_color():
     setup()
@@ -47,12 +48,10 @@ def get_color():
         duration = time.time() - start
         green = NUM_CYCLES / duration
 
-        if 14000 <= red <= 18000 and 14000 <= blue <= 17500 and 12000 <= green <= 15000:
+        if 13000 <= red <= 18000 and 10000 <= blue <= 17500 and 9000 <= green <= 12500:
             colors.append("B")
-        elif 12000 <= red <= 35000 and 16000 <= blue <= 35000 and 15001 <= green <= 35000:
+        elif 12000 <= red <= 35000 and 16000 <= blue <= 35000 and 15000 <= green <= 35000:
             colors.append("W")
-        elif 13000 <= red <= 20000 and 10000 <= blue <= 15500 and 9000 <= green <= 12000:
-            colors.append("N")
         else:
             colors.append("U")
         time.sleep(1)
@@ -62,3 +61,4 @@ def get_color():
     most_common_color = counter.most_common(1)[0][0]
     GPIO.cleanup()
     return most_common_color
+    
